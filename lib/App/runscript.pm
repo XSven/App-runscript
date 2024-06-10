03ad1a0fee9cbf5a2d3469acce7a3c51a4237e99
Sven Willenbuecher <sven.willenbuecher@gmx.de>
2024-06-10 15:04:44 +0200

    initial commit

---
+++ lib/App/runscript.pm
@@ -0,0 +1,77 @@
+use strict;
+use warnings;
+
+package Run::Scripts;
+
+our $VERSION = 'v1.0.0';
+
+use subs qw( main _croakf _which );
+
+use Config         qw( %Config );
+use File::Basename qw( dirname );
+use File::Spec     qw();
+
+# FIXME: the name of this should be changed because it isn't a real "main"
+# function
+sub main ( @ ) {
+  my ( $script ) = @_;
+
+  if ( File::Spec->file_name_is_absolute( $script ) ) {
+    _croakf "Script '%s' has no execute permission", $script unless -x $script;
+  } else {
+    $script = _which $script, 1;
+    _croakf "Cannot find script '%s' in PATH (%s)", $_[ 0 ], $ENV{ PATH } unless defined $script;
+  }
+  shift;
+
+  my $install_base     = dirname dirname $script;
+  my $install_priv_lib = File::Spec->catdir( $install_base, qw( lib perl5 ) );
+
+  _croakf "Lib directory '%s' derived from script name '%s' does not exist", $install_priv_lib, $script
+    unless -d $install_priv_lib;
+
+  return ( "-I$install_priv_lib", $script, @_ );
+}
+
+sub _croakf ( $@ ) {
+  require Carp;
+  @_ = ( ( @_ == 1 ? shift : sprintf shift, @_ ) . ', stopped' );
+  goto &Carp::croak;
+}
+
+sub _which ( $;$ ) {
+  my ( $executable, $abs_path ) = @_;
+
+  _croakf 'Cannot locate undefined executable file' unless defined $executable;
+
+  # path_sep refers to the command shell search PATH separator character
+  for ( split /$Config{ path_sep }/, $ENV{ PATH } ) { ## no critic (RequireExtendedFormatting)
+    my $file = File::Spec->catfile( $_, $executable );
+    if ( -x $file ) {
+      if ( $abs_path ) {
+        require Cwd;
+        return Cwd::abs_path( $file );
+      } else {
+        return $file;
+      }
+    }
+  }
+
+  return;
+}
+
+1;
+__END__
+
+https://stackoverflow.com/questions/3841322/how-do-i-load-libraries-relative-to-the-script-location-in-perl
+
+https://perldoc.perl.org/perlrun
+https://perldoc.perl.org/functions/exec
+
+
+https://metacpan.org/pod/Path::This
+https://metacpan.org/pod/lib::relative
+https://metacpan.org/pod/FindBin
+
+https://metacpan.org/pod/lib::root
+https://metacpan.org/pod/FindBin::libs
